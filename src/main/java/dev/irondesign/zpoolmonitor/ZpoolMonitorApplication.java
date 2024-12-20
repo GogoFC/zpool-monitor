d2660478115ba8a63e7e5a326e334341b6ea07c7
GogoFC <goranmuminovic@gmail.com>
2024-12-20 19:39:40 +0100

    Initial commit

---
+++ src/main/java/dev/irondesign/zpoolmonitor/ZpoolMonitorApplication.java
@@ -0,0 +1,13 @@
+package dev.irondesign.zpoolmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpoolMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZpoolMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/irondesign/zpoolmonitor/ZpoolMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.irondesign.zpoolmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZpoolMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
