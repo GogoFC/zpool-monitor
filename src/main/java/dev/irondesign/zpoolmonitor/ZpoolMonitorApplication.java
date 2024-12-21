84114a6e5ddae89bbdf099192046117b5562f49b
GogoFC <goranmuminovic@gmail.com>
2024-12-21 21:18:23 +0100

    remove websockets

--- src/main/java/dev/irondesign/zpoolmonitor/ZpoolMonitorApplication.java
+++ src/main/java/dev/irondesign/zpoolmonitor/ZpoolMonitorApplication.java
@@ -2,9 +2,6 @@ package dev.irondesign.zpoolmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
 @SpringBootApplication
 public class ZpoolMonitorApplication {
 
@@ -12,9 +9,6 @@ public class ZpoolMonitorApplication {
         SpringApplication.run(ZpoolMonitorApplication.class, args);
     }
 
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
-    }
+
 
 }
